efa51c56a3bebca8ac60c92d1857c9e6b1853b29
Violet Eldridge <github@violet-mica.com>
2023-10-28 21:01:41 -0700

    switch from pipenv to poetry reduce rate of spotify api calls by delaying 1 second before a repeat attempt start using logging instead of printing refactor, fix bugs

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ from twitchio import Channel, User, Message, Client
 from twitchio.ext import commands, routines, eventsub
 from twitchio.ext.commands import Command, Context
 
-import spotipy
+from spotipy.client import Spotify as Spotipy
 from spotipy.oauth2 import SpotifyOAuth
+
 
 from dotenv import load_dotenv
 
@@ -22,20 +23,19 @@ from utils import now, getenv
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
-# logging.basicConfig(level=logging.DEBUG)
 
 
-if os.getenv('USE_PYCHARM'):
-    """ 
-        Load the .env file so our environment variables are set, but only when using pycharm since it doesn't know how
-        Set USE_PYCHARM in the environment variables within pycharm
-        Set your variables in .env-template and rename it to .env
-        
-    """
-    load_dotenv(f'{os.getcwd()}/.env')
+#if os.getenv('USE_PYCHARM'):
+#    """ 
+#        Load the .env file so our environment variables are set, but only when using pycharm since it doesn't know how
+#        Set USE_PYCHARM in the environment variables within pycharm
+#        Set your variables in .env-template and rename it to .env
+#        
+#    """
+load_dotenv(f'{os.getcwd()}/.env')
 
 
-class Spotify(spotipy.Spotify):
+class Spotify(Spotipy):
     next_song_info_time: float
     current_song_info: str
     def __init__(self):
@@ -50,13 +50,27 @@ class Spotify(spotipy.Spotify):
         self.current_song_info = ''
 
     def get_current_song_str(self, force_response:bool=False) -> str:
-        remaining = self.next_song_info_time - time.time()
+        """
+        implement force_response
+        needed test cases:
+        * no song playing (and hasn't in a while so the api call will not return a current song)
+        """
         if self.next_song_info_time > 0 and time.time() < self.next_song_info_time:
-            return f"{self.current_song_info} - Current song ends in {remaining:.0f} seconds"
+            remaining = self.next_song_info_time - time.time()
+            return f"{self.current_song_info} - Ends in {remaining//60:.0f}:{remaining%60//1:>02.0f} seconds"
             # logger.warning(f"called too soon; wait until current song ends in {remaining:.1f} seconds")
             # return self.current_song_info if force_response else ''
 
-        pb = self.current_playback()
+        #TODO:separate function
+        pb = None
+        try:
+            pb = self.current_playback()
+        except Exception as e1:
+            time.sleep(1)
+            try:
+                pb = self.current_playback()
+            except Exception as e2:
+                logger.exception('Failed to get current playback information from spotify twice in a row; errors:\n%s\n%s', e1, e2)
         if not pb:
             logger.error("failed to get current playback info from spotify API")
             return ''
@@ -64,15 +78,15 @@ class Spotify(spotipy.Spotify):
         duration_ms = pb['item']['duration_ms']
         progress_ms = pb['progress_ms']
         remaining_ms = duration_ms - progress_ms
-        remaining_seconds = remaining_ms / 60
+        remaining_seconds = remaining_ms / 1000
         self.next_song_info_time = time.time() + remaining_seconds
 
         song_name = pb['item']['name']
         artists_str = ', '.join(a['name'] for a in pb['item']['artists'])
         full_str = f'{song_name} - {artists_str}'
         self.current_song_info = full_str
-        return f"{self.current_song_info} - Ends in {remaining:.0f} seconds"
-
+        remaining = remaining_seconds
+        return f"{self.current_song_info} - Ends in {remaining//60:.0f}:{remaining%60//1:>02.0f}"
 
 
 class TeraBot(commands.Bot):
@@ -90,7 +104,7 @@ class TeraBot(commands.Bot):
         self.spotify = spotify
         self.inbox = inbox
         self.outbox = outbox
-        print("loading commands :D")
+        logger.debug("loading commands :D")
         self.load_commands(commands_file)
 
 
@@ -101,7 +115,7 @@ class TeraBot(commands.Bot):
             try:
                 self.loaded_commands = json.load(f)
             except FileNotFoundError as fe:
-                print(f"Unable to load commands {fe}")
+                logger.warning(f"Unable to load commands {fe}")
 
         for k, v in self.loaded_commands.items():
             # TODO: Validation checking
@@ -109,7 +123,7 @@ class TeraBot(commands.Bot):
             _alias = v["alias"]
             _text = v["text"]
 
-            print(f"Loading command: {_cname}")
+            logger.debug(f"Loading command: {_cname}")
 
             def _make_function(name, _txt):
                 async def _command(ctx: Context):
@@ -126,16 +140,16 @@ class TeraBot(commands.Bot):
 
             new_cmd = _make_function(_cname, _text)
             setattr(self, _cname, new_cmd)
-            print(f"command {_cname} Loaded...")
+            logger.debug(f"command {_cname} Loaded...")
 
     # Called once the bot is ready, seems to be executed after event_join
     async def event_ready(self):
-        print(f'Logged in as | {self.nick}')
-        print(f'user id is | {self.user_id}')
+        logger.info(f'Logged in as | {self.nick}')
+        logger.info(f'user id is | {self.user_id}')
 
     # Called whenever someone joins the channel
     async def event_join(self, channel: Channel, user: User):
-        print(f'{datetime.datetime.now()} {self.nick} joined {channel.name}')
+        logger.info(f'{datetime.datetime.now()} {self.nick} joined {channel.name}')
 
     async def event_message(self, message: Message) -> None:
         """runs every time a message is sent"""
@@ -144,56 +158,91 @@ class TeraBot(commands.Bot):
         if message.echo:
             return
 
-        print(f'{now()}:{message.author.name}: "{message.content}"')
+        logger.info(f'{now()}:{message.author.name}: "{message.content}"')
+
+        ctx = await self.get_context(message)
 
         # first check if the message contains a command meant for the bot.
         # we have to call this explicitly since we're overwriting event_message.
-        await self.handle_commands(message)
+        await self.do_handle_command(message, ctx)
+        # logger.debug(f'{message.tags}')
 
-        # Get the context directly from the bot
-        ctx = await self.get_context(message)
-        # print(f'{message.tags}')
+        if self.is_moderator(message):
+            pass
+        elif self.is_streamer(message):
+            pass
+        elif self.is_viewer(message):
+            pass
 
-        if message.tags['user-type'] == 'mod':
-            # await ctx.send(f'Oh look its another mod.. how original : {message.author.name}')
-            print(f'Oh look its another mod.. how original : {message.author.name}')
+    async def do_handle_command(self, message:Message, ctx:Optional[Context]=None):
+        if ctx is None:
+            ctx = await self.get_context(message)
+
+        """
+        2023-10-01 10:41:01.617230:violet_revenant: "!song"
+        ctx <twitchio.ext.commands.core.Context object at 0x7f6bad5bbe50>
+        2023-10-01 10:41:01.647477 (('Connection aborted.', RemoteDisconnected('Remote end closed connection without response'))) sent by violet_revenant
+        Invalid command !song
+
+        2023-10-01 10:42:28.539541:violet_revenant: "!song"
+        ctx <twitchio.ext.commands.core.Context object at 0x7f6bad5cd790>
+        SONG STRING Two Dots - Lusine - Ends in 2:59
+        """
+        if not ctx.prefix:
+            logger.debug(
+                f"skipping command processing because `not ctx.prefix`: "
+                f"`not {ctx.prefix}`"
+            )
+        elif not ctx.is_valid:
+            logger.info(
+                f"skipping command processing because `not ctx.is_valid`: "
+                f"`not {ctx.is_valid}`"
+            )
+        elif ctx.command is None:
+            logger.debug("skipping command processing because `ctx.command is None`")
         else:
-            if 'broadcaster' not in message.tags['badges']:
-                # await ctx.send('regular user, how .. regular?')
-                print('regular user, how .. regular?')
-
-        # if 'tera' in message.content.lower():
-        #     await ctx.send("tera blah blah blah")
+            logger.info(f"command: {ctx.command}, {ctx.command.full_name}")
+            self.run_event("command_invoke", ctx)
+            await ctx.command(ctx)
 
     async def event_command_error(self, context: Context, error: Exception) -> None:
         # TODO: punish spammers of invalid commands. Maybe more than 5 within X seconds
         # TODO: Log to log file for bot
-        print(f'{now()} ({error.args[0]}) sent by {context.author.name}')
+        logger.info(f'{now()}:{context.author.name}:Failed to run command: ({error.args[0]})')
 
         # await context.send(f"Invalid command {context.message.content}")
-        print(f"Invalid command {context.message.content}")
+        # logger.info(f"Invalid command {context.message.content}")
+
+    def is_moderator(self, message:Message) -> bool:
+        return message.tags['user-type'] == 'mod'
+    def is_streamer(self, message:Message) -> bool:
+        return 'broadcaster' in message.tags['badges']
+    def is_viewer(self, message:Message) -> bool:
+        if self.is_moderator(message) or self.is_streamer(message):
+            return False
+        return 'broadcaster' not in message.tags['badges']
 
     # @routines.routine(seconds=30, iterations=2)
     # async def dosomething(self, arg: str):
-    #     print(f'Something happening here every 30s {arg}')
+    #     logger.info(f'Something happening here every 30s {arg}')
     #
     # @dosomething.after_routine
     # async def afterstuff(self):
-    #     print(f'This happened after the routine')
+    #     logger.info(f'This happened after the routine')
     #
     # @dosomething.before_routine
     # async def beforestuff(self):
-    #     print(f'this is doing some stuff before the routine')
+    #     logger.info(f'this is doing some stuff before the routine')
 
     # @commands.command()
     # async def hello(self, ctx: Context):
     #     await ctx.send(f'Hello {ctx.author.name}!')
     #
     #     users = await self.get_webhook_subscriptions()
-    #     print(f'{users}')
+    #     logger.info(f'helo command, self.get_webhook_subscriptions ("users"): {users}')
 
     @commands.command(name="register", aliases=['reg', 'signup', 'create'])
-    async def register(self, ctx: Context):
+    async def register(self, ctx: commands.Context):
         print(f'Registering a new user.... {ctx.author.name}')
         await ctx.send(f'Registered user {ctx.author.name}')
 
@@ -203,10 +252,11 @@ class TeraBot(commands.Bot):
         print(ctx.message.content)
 
     @commands.command(name="song")
-    async def reminder(self, ctx: Context):
+    async def song(self, ctx: Context):
         song_str = self.spotify.get_current_song_str()
         print("SONG STRING", song_str)
         if not song_str:
+            await ctx.send("Failed to get info from Spotify API")
             return
         await ctx.send(song_str)
 
@@ -231,9 +281,9 @@ def main():
     # print(f"{commands_json_filepath}")
     to_bot = Queue()
     from_bot = Queue()
-    bot = TeraBot(to_bot, from_bot, spotify, commands_json_filepath)
+    twitch_bot = TeraBot(to_bot, from_bot, spotify, commands_json_filepath)
 
-    bot_thread = threading.Thread(target=bot.run, daemon=True)
+    bot_thread = threading.Thread(target=twitch_bot.run, daemon=True)
     bot_thread.start()
 
     while True:
@@ -244,4 +294,17 @@ def main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    # quiet down other loggers
+    loggers = [
+        'twitchio.websocket',
+        'twitchio.client',
+        'urllib3.connectionpool',
+        #'spotipy.client',
+    ]
+    for log_name in loggers:
+        logging.getLogger(log_name).setLevel(level=logging.INFO)
+    logging.getLogger('spotipy.client').setLevel(level=logging.DEBUG)
+
     main()
